ff79d4d2fe1a93e0d03926326dca2801cb19ef7b
Shnnly <gprimila@gmail.com>
2022-08-01 19:53:55 -0300

    Finalização tarefa 1

---
+++ Tarefas-Semana-1/Tarefa-1.js
@@ -0,0 +1,11 @@
+let nome = "Guilherme"
+let email = "gprimila@gmail.com"
+let profissão = "Estudante"
+let idade = "14"
+let lugar = "Lapland, Finlandia"
+
+console.log(nome);
+console.log(email);
+console.log(profissão);
+console.log(idade);
+console.log(lugar);
