2523d785df5609952f00855789108aa656b45f06
Shnnly <gprimila@gmail.com>
2022-08-02 01:01:18 -0300

    Finalização tarefa 6

---
+++ Tarefas-Semana-1/Tarefa-6.js
@@ -0,0 +1,23 @@
+let numero1 = 1
+let numero2 = 2
+let numero3 = 3
+let numero4 = 4
+let numero5 = 5
+let numero6 = 6
+let numero7 = 7
+let numero8 = 8
+let numero9 = 9
+let numero10 = 10
+
+let a = 123
+
+console.log(a*numero1)
+console.log(a*numero2)
+console.log(a*numero3)
+console.log(a*numero4)
+console.log(a*numero5)
+console.log(a*numero6)
+console.log(a*numero7)
+console.log(a*numero8)
+console.log(a*numero9)
+console.log(a*numero10)
