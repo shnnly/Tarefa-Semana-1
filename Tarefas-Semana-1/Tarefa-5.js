2e5714b0ef55be0da5bbafec3c1e1da761df8a3e
Shnnly <gprimila@gmail.com>
2022-08-02 00:51:53 -0300

    Finalização tarefa 5

---
+++ Tarefas-Semana-1/Tarefa-5.js
@@ -0,0 +1,7 @@
+var nome = "Guilherme"
+var email = "gprimila@gmail.com"
+var profissão = "Estudante"
+var idade = "14"
+var lugar = "Lapsland, Finlandia"
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissão} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
