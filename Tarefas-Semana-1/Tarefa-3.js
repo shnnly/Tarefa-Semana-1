9ce29f78f54660709c94a3520026c3c0a0c33c14
Shnnly <gprimila@gmail.com>
2022-08-02 00:00:22 -0300

    Finalização tarefa 3

---
+++ Tarefas-Semana-1/Tarefa-3.js
@@ -0,0 +1,41 @@
+var nome = String
+var email = String
+var profissão = String
+var idade = Number 
+var lugar = String
+
+nome = "Guilherme"
+email = "gprimila@gmail.com"
+profissão = "Estudante"
+idade = "14"
+lugar = "Lapland, Finlandia"
+
+if (nome = String){ 
+    console.log(true)
+} else {
+ console.log(false)
+}
+
+if (email = String){ 
+    console.log(true)
+} else {
+ console.log(false)
+}
+
+if (profissão = String){ 
+    console.log(true)
+} else {
+ console.log(false)
+}
+
+if (idade == String){ 
+    console.log(true)
+} else {
+ console.log(false)
+}
+
+if (lugar = String){ 
+    console.log(true)
+} else {
+ console.log(false)
+}
