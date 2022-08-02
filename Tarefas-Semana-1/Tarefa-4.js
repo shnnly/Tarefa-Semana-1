c5dc252ae9948aa359e0e05da4306a5c32735c6f
Shnnly <gprimila@gmail.com>
2022-08-02 00:23:28 -0300

    Finalização tarefa 4

---
+++ Tarefas-Semana-1/Tarefa-4.js
@@ -0,0 +1,7 @@
+/*Dado que a distância entre a Terra e a Lua em milhas é de 238900
+coloque este valor em uma variável e em seguida exiba o valor 
+convertido para quilômetros*/
+
+let x = 238900
+
+console.log(x*1.60934)
