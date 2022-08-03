2f91fe2752974e958436e873c2bdd9e87f407c55
Shnnly <gprimila@gmail.com>
2022-08-03 20:24:32 -0300

    Finalização da tarefa 2

---
+++ Tarefa-Semana-2/Tarefa-2.js
@@ -0,0 +1,17 @@
+let comprador = "Rogerio"
+let produto = "'AntiMateria'"
+let endereço = "123Street"
+let vendedor = "Cleitinho"
+
+function entrega(params) {
+    console.log(`Olá, ${comprador}.`)
+    
+    console.log(`Fizemos a entrega do produto ${produto} no endereço ${endereço} com sucesso
+    O seu produto foi recebido por: ${comprador}
+    Esperamos que você esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que possamos ajudar.`)
+    
+    console.log(`Atenciosamente,
+    ${vendedor}`)
+}
+
+entrega()
