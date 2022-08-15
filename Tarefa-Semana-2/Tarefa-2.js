287d1a8d870f324c3136c0e51be781031df7cf4d
shnnly <gprimila@gmail.com>
2022-08-14 23:53:07 -0300

    Finalização da tarefa 3

--- Tarefa-Semana-2/Tarefa-2.js
+++ Tarefa-Semana-2/Tarefa-2.js
@@ -8,7 +8,7 @@ function entrega(params) {
     
     console.log(`Fizemos a entrega do produto ${produto} no endereço ${endereço} com sucesso
     O seu produto foi recebido por: ${comprador}
-    Esperamos que você esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que possamos ajudar.`)
+    Esperamos que você esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que possamos ajudarno`)
     
     console.log(`Atenciosamente,
     ${vendedor}`)
---
+++ Tarefa-Semana-2/Tarefa-3.js
@@ -0,0 +1,9 @@
+function ex3() {
+    min = 0
+    max = 10000
+    return Math.floor(Math.random(max - min + 1));
+}
+
+let numero = ex3()
+
+console.log(numero)
